0288aafc668d66bba63a436bc803a0e71719156b
petra1 <petra@access-for-all.ch>
2022-05-06 16:12:22 +0200

    Deleted some unnecessary code in Contact.java and HeadingsFragment.java.

--- app/src/main/java/ch/ritter1/apps/ademonstration/MainActivity.java
+++ app/src/main/java/ch/ritter1/apps/ademonstration/MainActivity.java
@@ -44,13 +44,14 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
-
+/**
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+ **/
 
     @Override
     public boolean onSupportNavigateUp() {
