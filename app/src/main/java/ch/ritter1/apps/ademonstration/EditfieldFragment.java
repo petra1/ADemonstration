af273481edea70ab1696f948980cd89aa19b8800
petra1 <petra@access-for-all.ch>
2022-05-20 11:25:57 +0200

    The edit field screen is in a working state.

--- app/src/main/java/ch/ritter1/apps/ademonstration/EditfieldFragment.java
+++ app/src/main/java/ch/ritter1/apps/ademonstration/EditfieldFragment.java
@@ -7,9 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class EditfieldFragment extends Fragment {
+    String lastname;
+    String firstname;
+    boolean clicked=false;
 
 
     public EditfieldFragment() {
@@ -27,7 +33,62 @@ public class EditfieldFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editfield, container, false);
+        View v = inflater.inflate(R.layout.fragment_editfield, container, false);
+        Button button_f_name = v.findViewById(R.id.bt_first_name);
+        Button button_l_name = v.findViewById(R.id.bt_last_name);
+        Button button_full_name = v.findViewById(R.id.bt_full_name);
+        TextView text_full_name = v.findViewById(R.id.text_full_name);
+         EditText editText_f_name = v.findViewById(R.id.editText_Fist_name);
+        EditText editText_l_name = v.findViewById(R.id.editText_Last_name);
+
+
+        button_f_name.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                        firstname = editText_f_name.getText().toString();
+                        if(clicked=true);
+                        button_f_name.setContentDescription(getString(R.string.first_name_send));
+
+
+
+                    }
+
+
+                }
+        );
+
+        button_l_name.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        lastname = editText_l_name.getText().toString();
+                        if(clicked=true);
+                        button_l_name.setContentDescription(getString(R.string.last_name_send));
+
+
+
+                    }
+
+
+                }
+        );
+        button_full_name.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        text_full_name.setText(firstname + " " + lastname);
+                        text_full_name.setAccessibilityLiveRegion(View.ACCESSIBILITY_LIVE_REGION_POLITE);
+
+                    }
+
+
+                }
+        );
+
+        return v;
     }
 }
