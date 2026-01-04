b0e308c56fb5061203426bc712816e8303d71e91
petra1 <your-email@example.com>
2026-01-04 13:07:47 +0100

    Implemented view binding in `GraphicsFragment`.

--- app/src/main/java/ch/ritter1/apps/ademonstration/graphics/GraphicsFragment.java
+++ app/src/main/java/ch/ritter1/apps/ademonstration/graphics/GraphicsFragment.java
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import ch.ritter1.apps.ademonstration.R;
+import ch.ritter1.apps.ademonstration.databinding.FragmentGraphicsBinding;
 
 
 public class GraphicsFragment extends Fragment {
 
+    private FragmentGraphicsBinding binding;
 
     public GraphicsFragment() {
         // Required empty public constructor
@@ -26,6 +27,13 @@ public class GraphicsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graphics, container, false);
+        binding = FragmentGraphicsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
